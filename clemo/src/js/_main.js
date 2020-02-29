ac3a772fd10410e7121f047c926cb717eabc2f3a
buryattvoydrug <buryattvoydrug@gmail.com>
2020-02-29 22:26:47 +0300

    asdsdhfgsdf

---
+++ clemo/src/js/_main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		 dots: true,
+         infinite: false,
+         arrows: false,
+         autoplay: true,
+         autoplaySpeed: 3000,
+         slidesToShow: 1
+     });
+    $('.best-team-slider').slick({
+         dots: true,
+         infinite: true,
+         arrows: true,
+         autoplay: true,
+         autoplaySpeed: 3000,
+         slidesToShow: 3,
+         slidesToScroll:3
+     });
+	$('#ex1').galereya({
+    wave: false,
+    modifier: 'ex1',
+    spacing: 5,
+    disableSliderOnClick: true,
+    load: function(next) {
+        $.getJSON('images.json', function(data) {
+            next(data);
+        });
+    }
+	});
+    $('.best-works').masonry({ //обозначаем общий контейнер
+      itemSelector: '.drid-item',
+      columnWidth: 364
+    });
+});
